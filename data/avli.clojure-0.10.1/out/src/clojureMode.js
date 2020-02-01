052b22bb35934e138dab6c09a5a2e8cb1134a084
Patrick Alaspää <patrick@Patricks-MBP.local>
2020-02-01 06:53:47 +0200

    Remove the pointless redownloadable stuff

--- data/avli.clojure-0.10.1/out/src/clojureMode.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CLOJURE_MODE = { language: 'clojure', scheme: 'file' };
-//# sourceMappingURL=clojureMode.js.map
--- data/shaunlebron.vscode-parinfer-0.6.1/src/config.js
+++
@@ -1,5 +0,0 @@
-const vscode = require('vscode')
-const workspace = vscode.workspace
-
-exports.useSmartMode = workspace.getConfiguration('parinfer').get('useSmartMode')
-exports.dimParenTrails = workspace.getConfiguration('parinfer').get('dimParenTrails')
